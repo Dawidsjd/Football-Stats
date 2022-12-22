f267f6795f7cc7dae79d05a6cca670c5e71efa91
Dawidsjd <dawid.buszta2005@gmail.com>
2022-12-22 22:37:42 +0100

    transfer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import './App.css';
 
 function App() {
+
+const [working, setWorking] = React.useState('DZIAŁA');
+
+//fetching the data
+fetch("https://v3.football.api-sports.io/fixtures/headtohead?h2h=33-34", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "v3.football.api-sports.io",
+        "x-rapidapi-key": "a11921fc86ef0ca55046a8565f4e22ec"
+    }
+})
+.then(response => response.json().then(data => {
+  console.log(data['response'][0]['teams']['away']['name'])
+    
+
+}))
+.catch(err => {
+    console.log(err);
+    setWorking('NIE DZIAŁA - too many request');
+});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div id='grid__container'>
+    <div className='stats__container'>
+      <div className='card'>
+      <div className='stats'>
+      <div id="wg-api-football-fixtures"
+                    data-host="v3.football.api-sports.io"
+                    data-refresh="60"
+                    data-date="2022-12-22"
+                    data-key="a11921fc86ef0ca55046a8565f4e22ec"
+                    data-theme="dark"
+                    data-show-errors="false"
+                    class="api_football_loader">
+        </div>
+        </div>
+      </div>
     </div>
+    </div>
+    
+</>
   );
 }
 
